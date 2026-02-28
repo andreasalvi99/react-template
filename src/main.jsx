6da0c9199a590149daa26ac89181cc689674943b
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-28 11:36:08 +0100

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 // ? Bootstrap
@@ -12,7 +12,7 @@ import "../src/assets/css/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  <App />,
+  // </StrictMode>,
 );
